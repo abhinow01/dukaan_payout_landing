5a6bf0d855a300c14e023cbae1156e68e3754969
abhinav khanna <kabhinav99@gmail.com>
2024-01-10 13:14:03 +0530

    added

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {  FiDownload } from 'react-icons/fi';
 import { TbArrowsSort } from "react-icons/tb";
+import { MdOutlineNavigateNext } from "react-icons/md";
+import { GrFormPrevious } from "react-icons/gr";
 // import Pagination from './Pagination';
 function Table() {
     const currentPage = 10;
@@ -36,28 +38,40 @@ function Table() {
             orderAmount: '₹1278.23',
             transactionFees: '₹22',
           },
-        //   {
-        //     orderId: '#281209',
-        //     orderDate: '7 July, 2023',
-        //     orderAmount: '₹1278.23',
-        //     transactionFees: '₹22',
-        //   },
-        //   {
-        //     orderId: '#281209',
-        //     orderDate: '7 July, 2023',
-        //     orderAmount: '₹1278.23',
-        //     transactionFees: '₹22',
-        //   },
-        //   {
-        //     orderId: '#281209',
-        //     orderDate: '7 July, 2023',
-        //     orderAmount: '₹1278.23',
-        //     transactionFees: '₹22',
-        //   }
+          {
+            orderId: '#281209',
+            orderDate: '7 July, 2023',
+            orderAmount: '₹1278.23',
+            transactionFees: '₹22',
+          },
+          {
+            orderId: '#281209',
+            orderDate: '7 July, 2023',
+            orderAmount: '₹1278.23',
+            transactionFees: '₹22',
+          },
+          {
+            orderId: '#281209',
+            orderDate: '7 July, 2023',
+            orderAmount: '₹1278.23',
+            transactionFees: '₹22',
+          },
+          {
+            orderId: '#281209',
+            orderDate: '7 July, 2023',
+            orderAmount: '₹1278.23',
+            transactionFees: '₹22',
+          },
+          {
+            orderId: '#281209',
+            orderDate: '7 July, 2023',
+            orderAmount: '₹1278.23',
+            transactionFees: '₹22',
+          }
           
       ];
       const header = (
-        <tr className="bg-gray-100">
+        <tr className="bg-gray-100 ">
           <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
             <span className="text-right">Order Id</span>
           </th>
@@ -77,18 +91,18 @@ function Table() {
       const dataRows = dummyData.map((item, index) => (
         
         <tr key={index} className="bg-white">
-          <td className="px-6 py-4 whitespace-nowrap">
+          <td className="px-6 py-2 whitespace-nowrap">
             <span className="text-blue-500">{item.orderId}</span>
           </td>
-          <td className="px-6 py-4 whitespace-nowrap text-right font-light">{item.orderDate}</td>
-          <td className="px-6 py-4 whitespace-nowrap text-right font-light">{item.orderAmount}</td>
-          <td className="px-6 py-4 whitespace-nowrap text-right font-light">{item.transactionFees}</td>
+          <td className="px-6 py-2 whitespace-nowrap text-right font-light">{item.orderDate}</td>
+          <td className="px-6 py-2 whitespace-nowrap text-right font-light">{item.orderAmount}</td>
+          <td className="px-6 py-2 whitespace-nowrap text-right font-light">{item.transactionFees}</td>
         </tr>
       ));
     
     return (
         <div className="container mx-auto h-full pb-8">
-      <div className="flex justify-between mb-4 bg-white">
+      <div className="flex flex-wrap justify-between mb-4 bg-white">
         <div className="flex px-4 py-2">
           <input
             type="text"
@@ -107,7 +121,7 @@ function Table() {
       </div>
       <table className="w-full px-4 pt-3 pb-4 border border-gray-200">
         <thead>{header}</thead>
-        <tbody className="divide-y divide-gray-200">
+        <tbody className=" divide-gray-200">
           {dataRows}
           {/* Add an extra empty row to ensure the last row renders correctly */}
           {/* <tr className="bg-white">
@@ -115,8 +129,21 @@ function Table() {
           </tr> */}
         </tbody>
       </table>
-      <div>
+      <div className='flex flex-row flex-wrap justify-center mx-auto px-24 py-4 pb-2'>
         {/* <Pagination currentPage={currentPage} totalPages={totalPages} /> */}
+        <div className=""><button className="bg-white px-4  flex items-center justify-center rounded-md border border-gray-200"><GrFormPrevious /> Previous</button></div>
+        <div className="px-2">1</div>
+        <div className="px-2">...</div>
+        <div className="px-2 border border-round rounded bg-blue-400">10</div>
+        <div className="px-2">11</div>
+        <div className="px-2">12</div>
+        <div className="px-2">13</div>
+        <div className="px-2">14</div>
+        <div className="px-2">15</div>
+        <div className="px-2">16</div>
+        <div className="px-2">17</div>
+        <div className="px-2">18</div>
+        <div className="px-2"><button className="bg-white px-4  flex items-center justify-center rounded-md border border-gray-200" >Next <MdOutlineNavigateNext /></button></div>
       </div>
     </div>
 
